071c0f764edbf176ab6663c22f504b8e7f771a3f
Dahye Kim <dahye.kim@nhnnext.org>
2015-09-29 19:05:22 +0900

    proj_jupiter 일단 완성. 목성의 자전 simulation

--- project_jupiter/project_jupiter/main.c
+++ project_jupiter/project_jupiter/main.c
@@ -11,23 +11,20 @@
 #include <GLUT/GLUT.h>
 #include <math.h>
 #include "SOIL.h"
-
 #define GL_PI 3.14159f
 static GLfloat xRot = 0.0f;
 static GLfloat yRot = 0.0f;
 static GLfloat zDistance = 0.0f;
+
+float sunRotate = 0.0f;
 float fElect1 = 0.0f;
 float fElect2 = 0.0f;
 GLUquadricObj *pSphere = NULL;
 
+GLuint earthTex;
 GLuint sunTex;
-GLuint spaceTex;
+GLuint moonTex;
 GLuint jupiterTex;
-
-GLfloat windowWidth = 800;
-GLfloat windowHeight = 800;
-int w1 = 0;
-int h1 = 0;
 
 GLuint loadTex(const char* filename)
 {
@@ -45,11 +42,35 @@ GLuint loadTex(const char* filename)
     return tex_ID;
 }
 
+void SetupRC()
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glEnable(GL_DEPTH_TEST);
+    glFrontFace(GL_CCW);
+    glEnable(GL_CULL_FACE);
+    
+    /* for lighting on texture mapping
+     * http://stackoverflow.com/questions/27712437/opengl-how-do-i-affect-the-lighting-on-a-textured-plane 참고.
+     */
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
+    
+    
+    /* for texture mapping
+     * http://stackoverflow.com/questions/25592313/opengl-texturing-sphere-using-soil 참고.
+     */
+    pSphere = gluNewQuadric();
+    gluQuadricDrawStyle(pSphere, GLU_FILL);
+    gluQuadricNormals(pSphere, GLU_SMOOTH);
+    gluQuadricTexture(pSphere, GLU_TRUE);
+}
+
 void initTex()
 {
     sunTex = loadTex("resources/sun.bmp");
-    spaceTex = loadTex("resources/space.bmp");
-    jupiterTex = loadTex("resources/jpiter.bmp");
+    earthTex = loadTex("resources/earth.bmp");
+    moonTex = loadTex("resources/moon.bmp");
+    jupiterTex = loadTex("resources/jupiter.bmp");
 }
 
 void TimerFunc(value)
@@ -58,11 +79,62 @@ void TimerFunc(value)
     glutTimerFunc(100, TimerFunc, 1);
 }
 
-void reshape(int w, int h)
+void lightSetting (GLfloat * amb, GLfloat * diff) {
+    glEnable(GL_LIGHTING);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diff);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+}
+
+void RenderScene()
 {
-    w1 = w;
-    h1 = h;
-    glViewport(0, 0, w, h);
+    // TODO. 카메라 위치 바꾸기...
+    
+    //multi light이 잘 안되서, 태양의 amb light값만 높임.
+    GLfloat amb[] = {0.001f, 0.001f,0.001f};
+    GLfloat diff[] = { 1.0f,1.0f,1.0f};
+    lightSetting(amb, diff);
+    
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glTranslatef(0.0f, 0.0f, 0.0f);
+    glPushMatrix();
+    
+    glEnable(GL_TEXTURE_2D);
+    glEnable(GL_DEPTH_TEST); // TODO. 왜 SetupRC에도 있는데.. RenderScene에도 있어야 할까?
+    glBindTexture(GL_TEXTURE_2D, jupiterTex);
+    glRotatef(sunRotate, 0.0f, 1.0f, 0.0f);// 목성 자전
+    glColor3ub(255, 255, 255);
+    gluSphere(pSphere, 50.0f, 25, 25); //glutSolidSphere로 그리면 texture mapping이 안된다!
+    
+//    GLfloat amb2[] = {1.0f,1.01f,1.01f};
+//    lightSetting(amb2, diff);
+//    glBindTexture(GL_TEXTURE_2D, sunTex);
+//    glPopMatrix();
+//    glRotatef(fElect1, 0.0f,1.0f,0.0f);
+//    glTranslatef(100.0f,0.0f,0.0f);
+//    glColor3ub(255, 255, 255);
+//    gluSphere(pSphere, 6.0f, 18, 18);
+//    glPushMatrix();
+    
+    sunRotate += 5.0f;
+    fElect1 += 5.0f;
+    fElect2 += 5.0f;
+    
+    if(sunRotate > 360.0f)
+        sunRotate = 0.0f;
+    
+    if(fElect1 > 360.0f)
+        fElect1 = 0.0f;
+    
+    if(fElect2 > 360.0f)
+        fElect2 = 0.0f;
+    
+    glPopMatrix();
+    glutSwapBuffers();
 }
 
 void KeyControl(int key, int x, int y)
@@ -88,147 +160,39 @@ void KeyControl(int key, int x, int y)
     glutPostRedisplay();
 }
 
-void orthogonalStart()
+void ChangeSize(int w, int h)
 {
+    GLfloat lightPos0[] = {60.0f, 60.0f, 100.0f, 1.0f };
+    //    GLfloat lightPos1[] = {-20.0f, 0.0f, -100.0f, 1.0f };
+    GLfloat nRange = 100.0f;
+    glViewport(0,0,w,h);
     glMatrixMode(GL_PROJECTION);
-    glPushMatrix();
     glLoadIdentity();
-    gluOrtho2D(-w1/2, w1/2, -h1/2, h1/2);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-void orthogonalEnd()
-{
-    glMatrixMode(GL_PROJECTION);
-    glPopMatrix();
-    glMatrixMode(GL_MODELVIEW);
-}
-
-
-GLuint texture = 0;
-
-void background()
-{
-    glBindTexture( GL_TEXTURE_2D, texture );
-    orthogonalStart();
+    if(w<=h){
+        glOrtho(-nRange,nRange,-nRange*h/w,nRange*h/w, -nRange*2.0f, nRange*2.0f);
+    }
+    else{
+        glOrtho(-nRange*w/h,nRange*w/h,-nRange,nRange, -nRange*2.0f,nRange*2.0f);
+    }
+    glLightfv(GL_LIGHT0, GL_POSITION,lightPos0);
+    //    glLightfv(GL_LIGHT1, GL_POSITION,lightPos1);
     
-    // texture width/height
-    const int iw = windowWidth;
-    const int ih = windowHeight;
-    
-    glPushMatrix();
-    glTranslatef( -iw/2, -ih/2, 0 );
-    glBegin(GL_QUADS);
-    glTexCoord2i(0,0); glVertex2i(0, 0);
-    glTexCoord2i(1,0); glVertex2i(iw, 0);
-    glTexCoord2i(1,1); glVertex2i(iw, ih);
-    glTexCoord2i(0,1); glVertex2i(0, ih);
-    glEnd();
-    glPopMatrix();
-    
-    orthogonalEnd();
-}
-
-void drawSolarSystem() {
-    
-    static GLfloat fElect1=0.0f;
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-    glTranslatef(0.0f, 0.0f, 0.0f);
-    
-    glEnable(GL_TEXTURE_2D);
-    glEnable(GL_DEPTH_TEST);
-    
-    glBindTexture(GL_TEXTURE_2D, sunTex);
-    glColor3ub(255, 230, 80);
-    gluSphere(pSphere, 20.0f, 18, 18);
-    glPushMatrix();
-    
-    glBindTexture(GL_TEXTURE_2D, sunTex);
-    glRotatef(fElect1, 0.0f,1.0f,0.0f);
-    glTranslatef(90.0f,0.0f,0.0f);
-    glColor3ub(65, 149, 192);
-    gluSphere(pSphere, 6.0f, 18, 18);
-    glPushMatrix();
-    
-    glBindTexture(GL_TEXTURE_2D, sunTex);
-    glRotatef(fElect2, 0.0f,1.0f,0.0f);
-    glTranslatef(10.0f,0.0f,0.0f);
-    glColor3ub(232,160,26);
-    gluSphere(pSphere, 2.0f, 18, 18);
-    
-    fElect1 += 5.0f;
-    fElect2 += 60.0f;
-    
-    if(fElect1 > 360.0f)
-        fElect1 = 0.0f;
-    
-    if(fElect2 > 360.0f)
-        fElect2 = 0.0f;
-    
-    glPopMatrix();
 }
 
-void SetupRC() {
-    /*기본 셋팅*/
-    glClearColor(0.0f, 0.0f, 0.0f, 1.0f );
-    glEnable(GL_DEPTH_TEST);
-    glFrontFace(GL_CCW);
-    glEnable(GL_CULL_FACE);
-    
-    /* for lighting on texture mapping
-     * http://stackoverflow.com/questions/27712437/opengl-how-do-i-affect-the-lighting-on-a-textured-plane 참고.
-     */
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE,GL_MODULATE );
-    
-    
-    /* for texture mapping
-     * http://stackoverflow.com/questions/25592313/opengl-texturing-sphere-using-soil 참고.
-     */
-    pSphere = gluNewQuadric();
-    gluQuadricDrawStyle(pSphere, GLU_FILL);
-    gluQuadricNormals(pSphere, GLU_SMOOTH);
-    gluQuadricTexture(pSphere, GLU_TRUE);
-    
-    /*light add*/
-    GLfloat amb[] = {0.1f,0.1f,0.1f};
-    GLfloat diff[] = { 1.0f,1.0f,1.0f};
-    glEnable(GL_LIGHTING);
-    glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, diff);
-    glEnable(GL_LIGHT0);
-    glEnable(GL_COLOR_MATERIAL);
-    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
-}
-
-void RenderScene()
-{
-    glClearColor (1.0,0.0,0.0,1.0);
-    glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glLoadIdentity();
-    glEnable( GL_TEXTURE_2D );
-    background();
-    drawSolarSystem();
-    gluLookAt (0.0, 0.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-
-    glutSwapBuffers();
-}
-
-int main(int argc, char **argv)
+int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-    
-    glutInitWindowSize(windowWidth,windowHeight);
-    glutCreateWindow("solar system with background");
-    
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800,800);
+    glutInitWindowPosition(0,0);
+    glutCreateWindow("Solar system with texture");
+    glutSpecialFunc(KeyControl);
+    glutReshapeFunc(ChangeSize);
     glutDisplayFunc(RenderScene);
-    glutReshapeFunc(reshape);
-    //glutSpecialFunc(KeyControl);
-    texture = loadTex("resources/galaxy.png");
-    //이동하는 것을 보려면 glutTimerFunc에 걸린 주석을 풀면 된다. 화살표 키로도 움직이는 것을 볼 수있다.
-    //glutTimerFunc(33,TimerFunc,1);
+    initTex();
+    glutTimerFunc(33,TimerFunc,1);
     SetupRC();
     glutMainLoop();
     return 0;
